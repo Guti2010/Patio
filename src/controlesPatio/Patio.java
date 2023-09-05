7ae03eb0ad87336b7425e95cdb10d13d32947ffd
Alejandro Gutiérrez <alejandrogutierrezchaves@gmail.com>
2023-09-05 11:55:46 -0600

    cmabios leves

--- src/controlesPatio/Patio.java
+++ src/controlesPatio/Patio.java
@@ -1,5 +1,34 @@
 package controlesPatio;
 
 public class Patio {
-
+	public void establecerElementos() {
+		
+	}
+	
+	public void inundarPatio() {
+		
+	}
+	public void enviarPlagas() {
+		
+	}
+	
+	public void regarElementos() {
+		
+	}
+	
+	public void crecerPasto() {
+		
+	}
+	
+	public void cortarPasto() {
+		
+	}
+	
+	public void fumigarPlantas() {
+		
+	}
+	
+	public void filtrarAgua() {
+		
+	}
 }
